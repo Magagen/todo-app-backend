a9d7b831a7ffb77e20da14f903a53a9746525e4e
makar <makar20158@gmail.com>
2026-03-21 02:00:46 +0300

    task 1 complete

--- main.py
+++ main.py
@@ -31,14 +31,26 @@ class TaskUpdate(BaseModel):
     title: str | None = None
     completed: bool | None = None
 
+class BookPost(BaseModel):
+    book: str
+
 
 tasks: list[Task] = []
+book : str = ""
 
+@app.post("/book", response_model=str, status_code=status.HTTP_200_OK)
+def post_book(payload: BookPost):
+    global book
+    book = payload.book
+    return book
 
-@app.get("/tasks", response_model=list[Task])
+@app.get("/tasks", response_model=str)
 def get_tasks():
     """Получить список задач"""
-    return tasks
+    global book
+    if book:
+        return "Любимая книга " + book
+    return "Любимой книги нет"
 
 
 @app.post("/tasks", response_model=Task, status_code=status.HTTP_201_CREATED)
